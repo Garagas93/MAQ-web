41c48a681b93f5005c9a0cd10e9a2e4a176f1ecb
Marco Minelli <m.minelli012@studenti.unibs.it>
2017-06-19 12:22:38 +0200

    Update

--- index.php
+++ index.php
@@ -5,25 +5,29 @@
 	<link rel="stylesheet" type="text/css" href="utility/bootstrap-3.3.7/dist/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/login_page.css">
 	<script type="text/javascript" src="js/global_config.js"></script>
+	<script type="text/javascript" src="js/login_page.js"></script>
 	<script type="text/javascript" src="utility/jquery-3.2.1.js"></script>
 </head>
+<?php 
+	session_start();
+?>
 <body onload="load_language(document.title)">
 	<div class="container ">
 	    <div class="row">
-	        <div class="col-md-offset-6 col-md-6">
+	        <div id="form-login" class="col-md-offset-6 col-md-6">
 	            <div class="form-login">
-	            <h4 id="login_header"></h4>
-	            <label class="control-label" for="username" id="login_username"></label>
-	            <input type="text" id="username" class="form-control input-sm chat-input"  required/>
-	            </br>
-	             <label class="control-label" for="password" id="login_password"></label>
-	            <input type="text" id="password" class="form-control input-sm chat-input" required/>
-	            </br>
-	            <div class="wrapper">
-	            <span class="group-btn">     
-	                <button id ="login_button" class="btn btn-primary btn-md"></button>
-	            </span>
-	            </div>
+		            <h4 id="login-header"></h4>
+		            <label class="control-label" for="username" id="login-username"></label>
+		            <input type="text" id="username" class="form-control input-sm chat-input"  required></input>
+		            </br>
+		            <label class="control-label" for="password" id="login-password"></label>
+		            <input type="text" id="password" class="form-control input-sm chat-input" required></input>
+		            </br>
+		            <div class="wrapper">
+			            <span class="group-btn">     
+			                <button id ="login-button" onclick="login();" class="btn btn-primary btn-md"></button>
+			            </span>
+	            	</div>
 	            </div>      
 	        </div>
 	    </div>
